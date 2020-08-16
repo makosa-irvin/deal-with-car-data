be25379fd3e446204f41ff8d9fc36f2353eb87de
Grey <makosa.irvin@gmail.com>
2020-08-17 00:32:26 +0300

    the graph

--- mpgovertime.py
+++ mpgovertime.py
@@ -55,47 +55,109 @@ def readnplot(filesname):
     stopTime = time_periods[-1]
     time_diff = stopTime - startTime
     total_time = time_diff.days
-    print(total_time)
+    #print(total_time)
     time_periods = time_periods[::-1]
     #highest mpg
     highest_mpg = 0
     for i in mpgList:
         if i > highest_mpg:
             highest_mpg = i
-    print(highest_mpg)
-    #plot using turtle
-    t = turtle.Turtle()
-    screen = t.getscreen()
-    screen.tracer(100)
+    #print(highest_mpg)
 
-    #mark the points
-    screen.setworldcoordinates(0,0,total_time,highest_mpg)
-    t.penup()
-    t.goto(0,mpgList[0])
-    t.pendown()
-    for j in range(len(mpgList)-1):
-        time_diffx = stopTime - time_periods[j]
-        
-        x = time_diffx.days
-        y = mpgList[j]
-        print(x)
+    return total_time, highest_mpg, mpgList, stopTime,time_periods
 
-        #t.penup()
-        t.goto(x,y)
-        #t.pendown
-        t.dot()
+#for nissan
+nissan_file = open("NissanVersa.csv","r")   
+nissan_totalTime,nissan_highestMPG, nissan_mpgList, nissan_stopTime,nissan_timePeriods = readnplot(nissan_file) 
+#toyota_file.close()
 
-    screen.update()
-    turtle.mainloop()
+#for toyota
+toyota_file = open("Toyota4Runner.csv","r")   
+toyota_totalTime,toyota_highestMPG, toyota_mpgList, toyota_stopTime,toyota_timePeriods = readnplot(toyota_file) 
+#toyota_file.close()
 
-# nissan_file = open("NissanVersa.csv","r")   
-# readnplot(nissan_file) 
-# nissan_file.close()
-
-# toyota_file = open("Toyota4Runner.csv","r")   
-# readnplot(toyota_file) 
-# toyota_file.close()
-
+#for suzuki
 suzuki_file = open("SuzukiS40.csv","r")   
-readnplot(suzuki_file) 
-suzuki_file.close()
+suzuki_totalTime,suzuki_highestMPG, suzuki_mpgList, suzuki_stopTime,suzuki_timePeriods = readnplot(suzuki_file) 
+#suzuki_file.close()
+
+
+#draw turtle
+t = turtle.Turtle()
+screen = t.getscreen()
+screen.tracer(100)
+
+#write text
+# t.penup()
+# t.setpos(0,0)
+# t.pendown()
+
+#mark the points
+#for nissan
+screen.setworldcoordinates(0,0,nissan_totalTime,nissan_highestMPG)
+t.penup()
+t.goto(0,nissan_mpgList[0])
+t.pendown()
+t.pencolor('green')
+for j in range(len(nissan_mpgList)-1):
+    time_diffx = nissan_stopTime - nissan_timePeriods[j]
+    
+    x = time_diffx.days
+    y = nissan_mpgList[j]/2
+    
+
+    #t.penup()
+    t.goto(x,y)
+    #t.pendown
+    t.dot()
+
+#for toyota
+#screen.setworldcoordinates(0,0,toyota_totalTime,toyota_highestMPG)
+t.penup()
+t.goto(0,toyota_mpgList[0])
+t.pendown()
+t.pencolor('blue')
+for j in range(len(toyota_mpgList)-1):
+    time_diffx = toyota_stopTime - toyota_timePeriods[j]
+    
+    x = time_diffx.days
+    y = toyota_mpgList[j]/2
+    
+
+    #t.penup()
+    t.goto(x,y)
+    #t.pendown
+    t.dot()
+
+#for suzuki
+#screen.setworldcoordinates(0,0,suzuki_totalTime,suzuki_highestMPG)
+t.penup()
+t.goto(0,suzuki_mpgList[0])
+t.pendown()
+t.pencolor('red')
+for j in range(len(suzuki_mpgList)-1):
+    time_diffx = suzuki_stopTime - suzuki_timePeriods[j]
+    
+    x = time_diffx.days
+    y = suzuki_mpgList[j]/2
+    
+
+    #t.penup()
+    t.goto(x,y)
+    #t.pendown
+    t.dot()
+
+#the x and y axis
+t.pencolor('black')
+t.pensize(3)
+t.penup()
+t.goto(0,300)
+t.pendown()
+t.left(270)
+t.forward(300)
+t.left(90)
+t.forward(300)
+
+
+screen.update()
+turtle.mainloop()
